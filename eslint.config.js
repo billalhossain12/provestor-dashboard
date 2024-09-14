5191b5fa5ab4124703cd34fe72c13528e0c3574e
billalhossain12 <billalhossain343434@gmail.com>
2024-09-14 20:33:40 +0600

    set up basic folder structure and installed several more required libraries and packages

--- eslint.config.js
+++ eslint.config.js
@@ -1,28 +1,26 @@
-import js from '@eslint/js'
-import globals from 'globals'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
-import tseslint from 'typescript-eslint'
+import globals from 'globals';
+import pluginJs from '@eslint/js';
+import tseslint from 'typescript-eslint';
 
-export default tseslint.config(
-  { ignores: ['dist'] },
-  {
-    extends: [js.configs.recommended, ...tseslint.configs.recommended],
-    files: ['**/*.{ts,tsx}'],
-    languageOptions: {
-      ecmaVersion: 2020,
-      globals: globals.browser,
-    },
-    plugins: {
-      'react-hooks': reactHooks,
-      'react-refresh': reactRefresh,
-    },
-    rules: {
-      ...reactHooks.configs.recommended.rules,
-      'react-refresh/only-export-components': [
-        'warn',
-        { allowConstantExport: true },
-      ],
-    },
-  },
-)
+export default [
+	{
+		settings: { react: { version: 'detect' } },
+
+		ignores: ['**/node_modules/', './dist'],
+
+		languageOptions: { globals: globals.browser },
+
+		files: ['**/*.{js,mjs,cjs,ts,jsx,tsx}'],
+
+		rules: {
+			'no-unused-vars': 'error',
+			'no-undef': 'error',
+			'no-console': 'warn',
+			'no-unused-expressions': 'error',
+			'prefer-const': 'error',
+		},
+	},
+
+	pluginJs.configs.recommended,
+	...tseslint.configs.recommended,
+];
