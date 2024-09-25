ab5f8ac7e9622bd76c42459a38515b288e4aa87b
billalhossain12 <billalhossain343434@gmail.com>
2024-09-25 11:37:14 +0600

    added property type button with arrow down icon

--- src/pages/Reports/PropertyReports/PropertyReports.tsx
+++ src/pages/Reports/PropertyReports/PropertyReports.tsx
@@ -1,20 +1,27 @@
-import ProgressAndLineChart from "./ProgressAndLineChart";
-import PropertyReportBarchart from "./PropertyReportBarchart";
-import PropertyReportCards from "./PropertyReportCards";
-import PropertyReportPieChart from "./PropertyReportPieChart";
+import { Icon } from '@iconify/react/dist/iconify.js';
+import ProgressAndLineChart from './ProgressAndLineChart';
+import PropertyReportBarchart from './PropertyReportBarchart';
+import PropertyReportCards from './PropertyReportCards';
+import PropertyReportPieChart from './PropertyReportPieChart';
 
 export default function PropertyReports() {
 	return (
 		<main>
-			<h3 className="text-[1.25rem] font-medium text-[#45464E] mb-[2rem]">
-				Property Reports
-			</h3>
-			<PropertyReportCards/>
-			<div className='flex lg:flex-row flex-col items-center gap-10 mb-[2.5rem] bg-white shadow-custom'>
-			<PropertyReportBarchart />
-			<PropertyReportPieChart/>
+			<div className="mb-[2rem] flex md:flex-row flex-col justify-between md:items-center md:gap-0 gap-3">
+				<h3 className="text-[1.25rem] font-medium text-[#45464E]">
+					Property Reports
+				</h3>
+				<button className="bg-primary w-[170px] px-3 py-2 text-white flex items-center justify-between gap-3">
+					<p>Property Type </p>
+					<Icon className='text-[1.3rem]' icon="solar:alt-arrow-down-line-duotone" />
+				</button>
 			</div>
-			<ProgressAndLineChart/>
+			<PropertyReportCards />
+			<div className="flex lg:flex-row flex-col items-center gap-10 mb-[2.5rem] bg-white shadow-custom">
+				<PropertyReportBarchart />
+				<PropertyReportPieChart />
+			</div>
+			<ProgressAndLineChart />
 		</main>
 	);
 }
