be1fb6b5f373a6ef4397c3800a5acdbe0b561a07
billalhossain12 <billalhossain343434@gmail.com>
2024-09-25 22:08:13 +0600

    added net returns and block investor buttons with responsive styles

--- src/pages/Investors/InvestorViewPanel.tsx
+++ src/pages/Investors/InvestorViewPanel.tsx
@@ -5,7 +5,8 @@ export default function InvestorViewPanel() {
 	const navigate = useNavigate();
 	return (
 		<main>
-			<div className="flex items-center md:gap-[2rem] gap-[1rem] mb-[3rem]">
+			<div className='flex md:flex-row flex-col md:justify-between md:items-center md:gap-0 gap-3 mb-[3rem]'>
+            <div className="flex items-center md:gap-[2rem] gap-[1rem]">
 				<div
 					onClick={() => navigate(-1)}
 					className="flex items-center md:gap-3 gap-2 border-[1px] border-gray-300 md:px-4 px-2 py-1 cursor-pointer"
@@ -17,6 +18,11 @@ export default function InvestorViewPanel() {
 					Investor ID : 12345
 				</h3>
 			</div>
+            <div className='flex md:flex-row flex-col md:items-center gap-3'>
+                <button className='md:px-8 px-5 py-2 bg-primary text-white'>Add dividends and net returns</button>
+                <button className='md:px-8 px-5 py-2 bg-red-500 text-white'>Block Investor</button>
+            </div>
+            </div>
             <Outlet/>
 		</main>
 	);
