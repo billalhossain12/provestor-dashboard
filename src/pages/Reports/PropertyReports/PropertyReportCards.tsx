3a984c4f6a1585bfde34706b6557fad6b1a84063
billalhossain12 <billalhossain343434@gmail.com>
2024-09-25 11:17:30 +0600

    use relevant icon property card

--- src/pages/Reports/PropertyReports/PropertyReportCards.tsx
+++ src/pages/Reports/PropertyReports/PropertyReportCards.tsx
@@ -1,4 +1,4 @@
-import { Icon } from "@iconify/react/dist/iconify.js";
+import { Icon } from '@iconify/react/dist/iconify.js';
 
 export default function PropertyReportCards() {
 	return (
@@ -11,7 +11,7 @@ export default function PropertyReportCards() {
 					<div className="bg-primary inline-block p-1">
 						<Icon
 							className="text-[1.5rem] text-white border-[1px] border-white md:w-[3rem] w-[2rem] md:h-[3rem] h-[2rem]"
-							icon="heroicons:user-group-solid"
+							icon="mdi:land-plots-marker"
 						/>
 					</div>
 				</div>
@@ -24,7 +24,7 @@ export default function PropertyReportCards() {
 					<div className="bg-primary inline-block p-1">
 						<Icon
 							className="text-[1.5rem] text-white border-[1px] border-white md:w-[3rem] w-[2rem] md:h-[3rem] h-[2rem]"
-							icon="heroicons:user-group-solid"
+							icon="mdi:land-plots-marker"
 						/>
 					</div>
 				</div>
@@ -56,9 +56,7 @@ export default function PropertyReportCards() {
 			{/* item- 4  */}
 			<div className="shadow-custom p-[1.25rem] bg-white">
 				<div className="flex items-center justify-between flex-wrap mb-2">
-					<h3 className="text-[#858585] font-semibold mb-3">
-						Total Profit
-					</h3>
+					<h3 className="text-[#858585] font-semibold mb-3">Total Profit</h3>
 					<div className="flex items-center gap-1 text-primary text-[14px]">
 						<Icon icon="material-symbols:trending-up" />
 						<p>+16.24 %</p>
