46f4df401d15af542359a1ee29aeb6c4f8234394
billalhossain12 <billalhossain343434@gmail.com>
2024-09-26 09:18:37 +0600

    fixed layout issue in the investor view page

--- src/pages/Investors/InvestorViewCards.tsx
+++ src/pages/Investors/InvestorViewCards.tsx
@@ -3,6 +3,76 @@ import profilePhoto from '../../assets/profile-photo.jpg';
 export default function InvestorViewCards() {
 	return (
 		<>
+			<section className="mb-[2.5rem]">
+				<div className="flex flex-wrap gap-[2rem] mb-[2rem]">
+					<img
+						className="md:w-[200px] w-full h-[155px] shadow-custom"
+						src={profilePhoto}
+						alt="Invesotr Image"
+					/>
+
+					<div className="bg-white shadow-custom p-[1rem] md:max-w-[500px] w-full">
+						<div className="flex md:items-center md:justify-between md:flex-row flex-col md:gap-0 gap-3">
+							<div className='flex items-center gap-3'>
+							<div className="bg-[#24B24B26] p-1 rounded-md text-primary w-[40px]">
+								<Icon className="text-[1.8rem]" icon="mdi:user" />
+							</div>
+							<div>
+								<h3 className="text-[1.25rem] font-semibold">Billal Hossain</h3>
+								<p className="text-[14px]">
+									<span className='text-gray-600'>Created on</span> <span className="font-medium">10 Jan 2024</span>
+								</p>
+							</div>
+							</div>
+							<div className="flex items-center gap-3 bg-[#24B24B26] rounded-md w-[100px] px-4 py-1">
+								<p className="bg-primary h-[10px] w-[10px] rounded-full"></p>
+								<p>Active</p>
+							</div>
+						</div>
+						<div className="flex md:justify-between md:items-center flex-wrap md:flex-row flex-col md:gap-0 gap-3 mt-8">
+							<div>
+								<h5 className="text-[#8B8D97] text-[14px]">Email ID:</h5>
+								<p className="font-medium">billal.hossain@gmail.com</p>
+							</div>
+							<div>
+								<h5 className="text-[#8B8D97] text-[14px]">Mobile Number:</h5>
+								<p className="font-medium">+8801311123533</p>
+							</div>
+						</div>
+					</div>
+				</div>
+
+
+
+
+				<div className="bg-white p-[1rem] shadow-custom flex-1">
+					<div className="grid xl:grid-cols-4 md:grid-cols-2 grid-cols-1 gap-5">
+						<div>
+							<h5 className="text-[#8B8D97] text-[14px]">Nationality:</h5>
+							<p className="font-medium">Bangladeshi</p>
+						</div>
+						<div>
+							<h5 className="text-[#8B8D97] text-[14px]">Date Of Birth:</h5>
+							<p className="font-medium">30/03/1997</p>
+						</div>
+						<div>
+							<h5 className="text-[#8B8D97] text-[14px]">Gender:</h5>
+							<p className="font-medium">Male</p>
+						</div>
+						<div>
+							<h5 className="text-[#8B8D97] text-[14px]">
+								Last Investment Date:
+							</h5>
+							<p className="font-medium">25/09/2024</p>
+						</div>
+						<div className="md:col-span-2">
+							<h5 className="text-[#8B8D97] text-[14px]">Address:</h5>
+							<p className="font-medium">Halder Bazar, Charvaga, Sharaitpur.</p>
+						</div>
+					</div>
+				</div>
+			</section>
+
 			<section className="grid xl:grid-cols-3 md:grid-cols-2 grid-cols-1 gap-[2rem] mb-[2.5rem]">
 				{/* item- 1  */}
 				<div className="shadow-custom p-[1.25rem] bg-white">
@@ -67,68 +137,6 @@ export default function InvestorViewCards() {
 							/>
 						</div>
 					</div>
-				</div>
-			</section>
-
-			<section className='mb-[2.5rem]'>
-				<div className="flex flex-wrap gap-[1rem] mb-[1rem]">
-					<img
-						className="md:w-[200px] w-full h-[155px]"
-						src={profilePhoto}
-						alt="Invesotr Image"
-					/>
-
-				<div className='bg-white p-[1rem] shadow-custom flex-1'>
-				<div className='grid xl:grid-cols-4 md:grid-cols-2 grid-cols-1 gap-5'>
-					<div>
-						<h5 className='text-[#8B8D97] text-[14px]'>Nationality:</h5>
-						<p className='font-medium'>Bangladeshi</p>
-					</div>
-					<div>
-						<h5 className='text-[#8B8D97] text-[14px]'>Date Of Birth:</h5>
-						<p className='font-medium'>30/03/1997</p>
-					</div>
-					<div>
-						<h5 className='text-[#8B8D97] text-[14px]'>Gender:</h5>
-						<p className='font-medium'>Male</p>
-					</div>
-					<div>
-						<h5 className='text-[#8B8D97] text-[14px]'>Last Investment Date:</h5>
-						<p className='font-medium'>25/09/2024</p>
-					</div>
-					<div>
-						<h5 className='text-[#8B8D97] text-[14px]'>Address:</h5>
-						<p className='font-medium'>Halder Bazar, Charvaga, Sharaitpur.</p>
-					</div>
-				</div>
-				</div>
-				</div>
-
-
-				<div className='bg-white shadow-custom p-[1rem] max-w-[600px]'>
-				<div className='flex md:items-center md:justify-between md:flex-row flex-col md:gap-0 gap-3'>
-					<div className="bg-[#24B24B26] p-1 rounded-md text-primary w-[40px]">
-						<Icon className="text-[1.8rem]" icon="mdi:user" />
-					</div>
-					<div>
-						<h3 className='text-[1.25rem] font-semibold'>Billal Hossain</h3>
-						<p className='text-[14px]'>Created on <span className='font-medium'>10 Jan 2024</span></p>
-					</div>
-					<div className='flex items-center gap-3 bg-[#24B24B26] rounded-md w-[100px] px-4 py-1'>
-						<p className='bg-primary h-[10px] w-[10px] rounded-full'></p>
-						<p>Active</p>
-					</div>
-				</div>
-				<div className='flex md:justify-between md:items-center flex-wrap md:flex-row flex-col md:gap-0 gap-3 mt-8'>
-					<div>
-						<h5 className='text-[#8B8D97] text-[14px]'>Email ID:</h5>
-						<p className='font-medium'>billal.hossain@gmail.com</p>
-					</div>
-					<div>
-						<h5 className='text-[#8B8D97] text-[14px]'>Mobile Number:</h5>
-						<p className='font-medium'>+8801311123533</p>
-					</div>
-				</div>
 				</div>
 			</section>
 		</>
